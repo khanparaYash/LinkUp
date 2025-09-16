2057cb6487de97236b98bd251246b6d47cc2b9f8
yk00004 <yashkhanpara11@gmail.com>
2025-09-16 17:03:09 +0530

    change env name

--- server/server.js
+++ server/server.js
@@ -1,93 +1,3 @@
-// import express from "express";
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// import cors from "cors";
-// import http from "http";
-// import { Server } from "socket.io";
-// import authRoutes from "./routes/auth.js";
-// import meetingRoutes from "./routes/meeting.js";
-// import morgan from "morgan";
-
-// dotenv.config();
-// const app = express();
-// const server = http.createServer(app);
-// app.use(morgan("dev"));
-
-// const io = new Server(server, {
-//   cors: { origin: process.env.CLIENT_URL || "*", methods: ["GET", "POST"] },
-// });
-
-// const users = {};
-// const socketToRoom = {};
-
-// io.on('connection', socket => {
-//      console.log("⚡ New socket connected:", socket.id);
-//     socket.on("join room", roomID => {
-//         console.log(`📢 ${socket.id} joined room: ${roomID}`);
-
-//         if (users[roomID]) {
-//             const length = users[roomID].length;
-//             if (length === 4) {
-//                 socket.emit("room full");
-//                 return;
-//             }
-//             users[roomID].push(socket.id);
-//         } else {
-//             users[roomID] = [socket.id];
-//         }
-//         socketToRoom[socket.id] = roomID;
-//         const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
-
-//         socket.emit("all users", usersInThisRoom);
-//     });
-
-//     socket.on("sending signal", payload => {
-//         console.log(`📤 Signal from ${socket.id} to ${payload.userToSignal}`);
-//         io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
-//     });
-
-//     socket.on("returning signal", payload => {
-//         console.log(`📤 Returning signal from ${socket.id}  to ${payload.callerID}`);
-//         io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
-//     });
-
-//     socket.on('disconnect', () => {
-//         console.log(`❌ Disconnected: ${socket.id}`);
-//         const roomID = socketToRoom[socket.id];
-//         let room = users[roomID];
-//         if (room) {
-//             room = room.filter(id => id !== socket.id);
-//             users[roomID] = room;
-//         }
-//     });
-
-// });
-
-// app.use(cors({ origin: process.env.CLIENT_URL || "*", credentials: true }));
-// app.use(express.json());
-
-// app.get("/", (req, res) => res.send("LinkUp API running"));
-// app.use("/api/auth", authRoutes);
-// app.use("/api/meetings", meetingRoutes);
-
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).json({ msg: "Something went wrong!" });
-// });
-
-// const PORT = process.env.PORT || 5000;
-// mongoose
-//   .connect(process.env.MONGO_URI, {})
-//   .then(() => {
-//     console.log("MongoDB connected");
-//     server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-//   })
-//   .catch((err) => {
-//     console.error("MongoDB connection error:", err.message);
-//     process.exit(1);
-//   });
-
-// --------------------------------------------------------------------------------------------------------------------
 
 import express from "express";
 import http from "http";
@@ -106,7 +16,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL ,
+    origin: process.env.CLIENT_URL ,
     credentials: true,
   })
 );
