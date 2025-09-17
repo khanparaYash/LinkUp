15dc4e3ab26cf00130bc75a0fe294a1b0fba0f17
yk00004 <yashkhanpara11@gmail.com>
2025-09-17 18:07:18 +0530

    change import

--- client/src/pages/WaitingForHost.jsx
+++ client/src/pages/WaitingForHost.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Hourglass, LogOut } from "lucide-react"
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/Button"
 
 const WaitingForHost = ({ leaveMeeting }) => {
   return (
