fffa3a8a958644956f0ff6779eb4a0bfe5709f70
yk00004 <yashkhanpara11@gmail.com>
2025-11-20 14:22:28 +0530

    change in padding

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,7 +33,7 @@ useEffect(() => {
 
       <Header />
       {/* Main content */}
-      <main className="flex-1 container mx-auto p-6 px-4 sm:px-6 lg:px-8 py-6 sm:py-8">
+      <main className="flex-1 container mx-auto sm:px-6 lg:px-8 py-6 sm:py-8">
         <Outlet />
        
       </main>
